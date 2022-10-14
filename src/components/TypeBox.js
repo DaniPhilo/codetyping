9f761cdcf2683b34fbe8928fdd35b8ef6c5099b4
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-10-14 20:17:11 +0200

    Remove Word component

--- src/components/TypeBox.js
+++ src/components/TypeBox.js
@@ -4,20 +4,26 @@ import '../assets/styles/TypeBox.css';
 import Word from './Word';
 
 export default function TypeBox({ text }) {
-    
+
     const splitText = text.split(' ');
-    
+
     const [wordIndex, setWordIndex] = useState(0);
     const [letterIndex, setLetterIndex] = useState(0);
     const [userInput, setUserInput] = useState(null);
 
+    const wordList = useRef([]);
+    const addWordRef = (ref) => wordList.current.push(ref);
+
+    const letterList = useRef([]);
+    const addLetterRef = (ref) => letterList.current.push(ref);
+
     const handleKeyDown = (event) => {
         const correct = splitText[wordIndex][letterIndex] === event.key;
-        console.log(correct);
+        if (correct) {
+            
+        }
         setLetterIndex(prev => prev += 1);
     }
-
-
 
     return (
         <section className="typebox-section">
@@ -25,9 +31,17 @@ export default function TypeBox({ text }) {
             <div className='input-container'>
                 <input name="user-input" id='user-input' onKeyDown={handleKeyDown} />
             </div>
+
             <div className="text-container">
                 {splitText.map((word, i) => {
-                    return <Word key={i} word={word} setLetterIndex={setLetterIndex} active={i === wordIndex ? true : false} />
+                    return (
+                        <div key={i} ref={addWordRef} className={i === wordIndex ? 'word current' : 'word'}>
+                            {word.split('').map((letter, i) => {
+                                return <p key={i} ref={addLetterRef} className='letter'>{letter}</p>
+                            }
+                            )}
+                        </div>
+                    )
                 })}
             </div>
 
