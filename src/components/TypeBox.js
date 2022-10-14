745953069807a07f2e381a5c51a4fba89d952ded
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-10-14 19:57:35 +0200

    Create ketDown event and check if user input matches letter

--- src/components/TypeBox.js
+++ src/components/TypeBox.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '../assets/styles/TypeBox.css';
 
 import Word from './Word';
 
 export default function TypeBox({ text }) {
-
+    
     const splitText = text.split(' ');
+    
+    const [wordIndex, setWordIndex] = useState(0);
+    const [letterIndex, setLetterIndex] = useState(0);
+    const [userInput, setUserInput] = useState(null);
+
+    const handleKeyDown = (event) => {
+        const correct = splitText[wordIndex][letterIndex] === event.key;
+        console.log(correct);
+        setLetterIndex(prev => prev += 1);
+    }
+
+
 
     return (
         <section className="typebox-section">
             <label>Type:</label>
             <div className='input-container'>
-                <input name="user-input" id='user-input' />
+                <input name="user-input" id='user-input' onKeyDown={handleKeyDown} />
             </div>
             <div className="text-container">
                 {splitText.map((word, i) => {
-                    return <Word key={i} word={word} />
+                    return <Word key={i} word={word} setLetterIndex={setLetterIndex} active={i === wordIndex ? true : false} />
                 })}
             </div>
-            
+
         </section>
     )
 }
--- src/components/Word.js
+++ src/components/Word.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import '../assets/styles/Word.css';
 
-import Letter from './Letter';
+// import Letter from './Letter';
 
-export default function Word({ word }) {
+export default function Word({ word, active, setLetterIndex }) {
+
+    const letterList = useRef([]);
+
+    const addRef = (ref) => letterList.current.push(ref);
 
     const wordSplit = word.split('');
 
     return (
-        <div className="word">
-            {wordSplit.map((letter, i) => <Letter key={i} letter={letter}/>)}
+        <div className={active ? 'word active' : 'word'}>
+            {wordSplit.map((letter, i) => <p key={i} ref={addRef} className='letter'>{letter}</p>)}
         </div>
     )
 }
