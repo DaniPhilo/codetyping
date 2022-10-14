611870a9d4dfbdcd3166e82609d138a77442cbd9
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-10-14 16:58:49 +0200

    Split text into words

--- src/components/TypeBox.js
+++ src/components/TypeBox.js
@@ -2,14 +2,23 @@ import React from 'react';
 import '../assets/styles/TypeBox.css'
 
 export default function TypeBox({ text }) {
+
+    const splitText = text.split(' ');
+
     return (
         <section className="typebox-section">
             <label htmlFor="input-box">Type:</label>
-            <div className='input-container'>
-                <p>{text}</p>
-
-                <textarea name="input-box" id="input-box" cols="90" rows="10"></textarea>
+            <div className="text-container">
+                <div className="words-container">
+                    {splitText.map(word => {
+                        return <div className='word'>{word}</div>
+                    })}
+                </div>
+                <div className='input-container'>
+                    <textarea name="input-box" id="input-box" cols="90" rows="10"></textarea>
+                </div>
             </div>
+
         </section>
     )
 }
