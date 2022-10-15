2448c0452354f03d78787ec44a0e3284d4e5ea81
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-10-15 19:37:57 +0200

    Create current word class and handle backspace
    Spacebar moves 'current' word class to next word.
    
    Backspace checks if letter pointer is in 'current' word before removing classes from letter and moving pointer back; if not, it doesn't delete anything.

--- src/components/TypeBox.js
+++ src/components/TypeBox.js
@@ -16,14 +16,25 @@ export default function TypeBox({ text }) {
         const currentLetter = letterRefList.current[letterIndex];
         const lastLetter = letterRefList.current[letterIndex - 1];
 
-        if (event.key === ' ' && !lastLetter.nextSibling) return
-
-        // if (event.key === 'Backspace' && currentLetter != currentLetter.parentElement.firstChild) {
-        //     console.log('backspace')
-        //     lastLetter.className = '';
-        //     setLetterIndex(prev => prev -= 1);
-        //     return
-        // }
+        // First letter sets first word as current word
+        if (letterIndex === 0) {
+            currentLetter.parentElement.classList.add('current');
+        }
+        // Spacebar moves current class to next word
+        if (event.key === ' ' && !lastLetter.nextSibling) {
+            lastLetter.parentElement.classList.remove('current');
+            return currentLetter.parentElement.classList.add('current');
+        }
+        // Backspace removes classes from letter only if word is current
+        if (event.key === 'Backspace') {
+            // If you are not in current word, you cannot delete
+            if (lastLetter.parentElement.classList[1] !== 'current') return
+            // Else, delete and move pointer back
+            lastLetter.className = '';
+            setLetterIndex(prev => prev -= 1);
+            return
+        }
+        // Check user input with current letter
         const correct = event.key === currentLetter.innerText;
         currentLetter.classList.add(correct ? 'correct' : 'incorrect');
         setLetterIndex(prev => prev += 1);
