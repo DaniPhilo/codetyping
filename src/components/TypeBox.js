1b6549dea97d57c447e7e01a1dce4c6181f89fc5
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-10-16 21:03:59 +0200

    x2: Change userResults to object.
    Calculate wpm unfinished.

--- src/components/TypeBox.js
+++ src/components/TypeBox.js
@@ -29,7 +29,10 @@ export default function TypeBox({ text, isGameOver, setGameStarted, setIsGameOve
         const numberOfMisplaced = refs.filter(ref => ref.classList.contains('misplaced')).map(p => p.innerText.slice(1)).join('').length;
         console.log('Misplaced letters: ' + numberOfMisplaced)
         console.log('Result: ' + [Math.round(100 - (numberOfIncorrect + numberOfMisplaced) * 100 / totalLetters)]);
+
         // Calculates wpm:
+        // TO DO: CREATE METHOD TO CALCULATE WPM BASED ON COUNTDOWN (15, 30, 45, 60). SEE: https://www.speedtypingonline.com/typing-equations
+        
         const wordsTyped = refs.filter(ref => ref.classList.length > 0).map(ref => ref.parentElement.lastChild === ref ? ref.innerText : null).join('').length - numberOfMisplaced;
         console.log('Words typed: ' + wordsTyped);
         setUserResults({ wordsTyped: totalLetters, accuracy: Math.round(100 - (numberOfIncorrect + numberOfMisplaced) * 100 / totalLetters), wpm: 60 / wordsTyped });
